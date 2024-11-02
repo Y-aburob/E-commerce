7d1b0d59901f925e0895895e23ad1ade516e05ee
yanal abu alrob <gonceloa@gmail.com>
2024-11-02 16:36:07 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
 import './App.css'
-import { motion, AnimatePresence } from 'framer-motion';
 import Home from './components/Home';
-import Nav from './components/Nav';
 
 export default function App() {
 
-  const [click, setClick] = useState(false)
 
   return (
     < >
